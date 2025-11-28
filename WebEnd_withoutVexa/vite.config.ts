79ad274a0caf87364b5eaf9dc251f5aa0694dc57
Cozymyz <70731447+Cozymyz@users.noreply.github.com>
2025-11-28 16:19:17 +0900

    创建了新项目WebEnd_withoutVexa
    这个就是不使用Vexa的项目，用来进行对比实验
    现在只是初步搭建，要尽快完成

---
+++ WebEnd_withoutVexa/vite.config.ts
@@ -0,0 +1,54 @@
+/*
+ * @Author: Meiyizhi
+ * @Date: 2025-11-28 16:12:25
+ * @LastEditTime: 2025-11-28 16:16:59
+ * @Description: 
+ */
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath } from 'url';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import { resolve } from 'path'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+		vue(),
+		AutoImport({
+			resolvers: [ElementPlusResolver()],
+			imports: ['vue', 'vue-router'],
+			dts: 'src/auto-imports.d.ts', // 生成自动导入的声明文件
+		}),
+		Components({
+			resolvers: [
+				ElementPlusResolver({
+					importStyle: 'sass', // 使用更高效的 SASS 样式
+				})
+			],
+			dts: 'src/components.d.ts', // 生成组件声明文件
+		})
+	],
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "./src"), // 设置为@ 则使用时为 "@/components/index.module.css"
+			"@components": resolve(__dirname, "./src/components"), // 使用时为 "@components/HelloWorld.vue"
+		},
+	},
+	server: {
+		// host: '0.0.0.0',
+		port: 8080, // 本地服务端口
+		https: false,
+		strictPort: true ,// 设为 true 时若端口已被占用则会直接退出，而不是尝试下一个可用端口
+		proxy: { // 代理
+			'/api': {
+				target: 'http://localhost:7788/',
+				changeOrigin: true,
+				rewrite: path => path.replace(/^\/api/, '')
+			}
+		}
+	}
+})
